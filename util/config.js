922079df889cd30e8796fd6b6f5a05b101d0c21a
changyanwei <1926174536@qq.com>
2021-04-20 17:16:26 +0800

    初始化项目

---
+++ util/config.js
@@ -0,0 +1,4 @@
+// 进一步降低耦合度
+export default {
+	host: 'http://localhost:3000'
+}
---
+++ util/request.js
@@ -0,0 +1,21 @@
+// 封装网络请求的功能函数
+
+// 导入配置服务器域名的文件，降低耦合度，因为服务器域名可能会变
+import config from './config.js';
+
+export default function (url, data = {}, method='GET') {
+	return new Promise((resolve, reject) => {
+		wx.request({
+			url: config.host + url,
+			data,
+			method,
+			success(res) {
+				resolve(res.data)
+			},
+			fail(err) {
+				console.log(err);
+				reject('出错啦！请重试')
+			}
+		})
+	})
+}
